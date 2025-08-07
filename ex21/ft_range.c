b7ba9ecf37d38233111c5bcb1ee900f75c0e9ffa
9x14S <Contact9x14S@proton.me>
2025-08-08 01:39:50 +0200

    small norminette fix

--- ex21/ft_range.c
+++ ex21/ft_range.c
@@ -16,13 +16,14 @@ int	*ft_range(int min, int max)
 {
 	int	abs_min;
 	int	i;
-	
+	int	*array;
+
 	if (min >= max)
 		return (NULL);
 	abs_min = min;
 	if (min < 0)
 		abs_min = -min;
-	int	*array = (int *)malloc(sizeof(int) * (max + abs_min));
+	array = (int *)malloc(sizeof(int) * (max + abs_min));
 	if (NULL == array)
 		return (NULL);
 	i = 0;
